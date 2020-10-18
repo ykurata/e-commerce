4612dad878c96b4e3fbb66d11b1a9a40f7a4cec1
ykurata <yasukokurata@gmail.com>
2020-10-17 23:01:38 -0400

    added image upload middleware

---
+++ server/routes/utils/upload.js
@@ -0,0 +1,23 @@
+const aws = require("aws-sdk");
+const multer = require("multer");
+const multerS3 = require("multer-s3");
+
+const s3 = new aws({
+  accessKeyId: process.env.AWS_ACCESS_KEY,
+  secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY
+});
+
+const upload = multer({
+  storage: multerS3({
+    s3: s3,
+    bucket: "yasuko-my-recipes",
+    metadata: function (req, file, cb) {
+      cb(null, { fieldName: file.fieldname });
+    },
+    key: function (req, file, cb) {
+      cb(null, file.originalname);
+    }
+  })
+});
+
+module.exports = upload;

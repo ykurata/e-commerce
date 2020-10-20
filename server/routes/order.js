6a1179e6df0c4b023617209fd85dd172a04c7ca6
ykurata <yasukokurata@gmail.com>
2020-10-19 23:45:16 -0400

    added post order route

--- server/routes/order.js
+++ server/routes/order.js
@@ -23,3 +23,33 @@ router.get("/my-orders", auth, async (req, res) => {
     console.log(err);
   }
 });
+
+// Get a specific order by order id 
+router.get("/:id", auth, async (req, res) => {
+  try {
+    const order = await Order.findOne({ _id: req.params.id });
+    return res.status(200).json(order);
+  } catch (err) {
+    console.log(err);
+  }
+});
+
+// Post a new order 
+router.post("/", auth, async (req, res) => {
+  try {
+    const newOrder = new Order({
+      orderItems: req.body.orderItems,
+      user: req.user,
+      shipping: req.body.shipping,
+      payment: req.body.payment,
+      itemsPrice: req.body.itemsPrice,
+      taxPrice: req.body.taxPrice,
+      shippingPrice: req.body.shippingPrice,
+      totalPrice: req.body.totalPrice,
+    });
+    const order = await newOrder.save();
+    return res.status(200).json(order);
+  } catch (err) {
+    console.log(err);
+  }
+});

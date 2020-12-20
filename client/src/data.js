89fe54df5a27f008543995119c1c16ac5af4f202
ykurata <yasukokurata@gmail.com>
2020-12-19 23:36:47 -0500

    rendered products data to the landing page

---
+++ client/src/data.js
@@ -0,0 +1,32 @@
+export default {
+  products: [
+    {
+      name: "Slim Shirts",
+      category: 'Shirts',
+      image: './images/d1.jpg',
+      price: 60,
+      brand: 'Nike',
+      rating: 4.5,
+      numReviews: 10
+    },
+    {
+      name: "Fit Shirts",
+      category: 'Shirts',
+      image: './images/d1.jpg',
+      price: 50,
+      brand: 'Nike',
+      rating: 4,
+      numReviews: 8
+    },
+    {
+      name: "Best Pants",
+      category: 'Pants',
+      image: './images/d1.jpg',
+      price: 70,
+      brand: 'Nike',
+      rating: 3.5,
+      numReviews: 6
+    },
+
+  ]
+}

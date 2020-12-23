d85c821b769177d80269fab67fbe1f5b0c437d42
ykurata <yasukokurata@gmail.com>
2020-12-23 13:23:12 -0500

    added styles to product details page

--- client/src/Screens/ProductScreen.js
+++ client/src/Screens/ProductScreen.js
@@ -3,8 +3,8 @@ import { Link } from 'react-router-dom';
 import data from '../data';
 
 const ProductScreen = (props) => {
-  console.log(props.match.params.id)
   const product = data.products.find(x => x._id === props.match.params.id);
+  console.log(product.image)
   return <div>
     <div>
       <Link to="/">Back to result</Link>

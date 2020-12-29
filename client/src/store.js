976a55d2d2acd3794e2e728567e72a1729c470d2
ykurata <yasukokurata@gmail.com>
2020-12-29 11:38:44 -0500

    created product constants and imported them in product reducers

--- client/src/store.js
+++ client/src/store.js
@@ -1,4 +1,5 @@
-import { createStore, combineReducers } from 'redux';
+import { createStore, combineReducers, applyMiddleware, compose } from 'redux';
+import thunk from 'redux-thunk';
 import { productListReducer } from './reducers/productReducers';
 
 const initialState = {};
@@ -6,6 +7,7 @@ const reducer = combineReducers({
   productList: productListReducer,
 
 })
-const store = createStore(reducer, initialState);
+const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const store = createStore(reducer, initialState, composeEnhancer(applyMiddleware(thunk)));
 
 export default store;

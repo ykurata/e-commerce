8a5d6ddc7e0efdd06b6755efdf675c730e2f5386
ykurata <yasukokurata@gmail.com>
2020-12-31 11:46:22 -0500

    created cart constants

--- client/src/reducers/cartReducers.js
+++ client/src/reducers/cartReducers.js
@@ -1,14 +1,18 @@
+import { CART_ADD_ITEM } from '../constants/cartConstants';
+
 function cartReducer(state = { cartItems: [] }, action) {
   switch (action.type) {
-    case ADD_TO_CART:
+    case CART_ADD_ITEM:
       const item = action.payload;
       const product = state.cartItems.find(x => x.product === item.product);
       if (product) {
         return {
           cartItems:
-            state.cartItems.map(x => x.product === product.product ? item : x);
+            state.cartItems.map(x => x.product === product.product ? item : x)
         }
       }
       return { cartItems: [...state.cartItems, item] }
   }
 }
+
+export { cartReducer }

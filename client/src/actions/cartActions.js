095213dfa7aa8d6e6caffd6af103e040a840fe51
ykurata <yasukokurata@gmail.com>
2021-01-02 17:27:14 -0500

    set remove item from cart action

--- client/src/actions/cartActions.js
+++ client/src/actions/cartActions.js
@@ -1,7 +1,8 @@
 import axios from 'axios';
+import Cookie from 'js-cookie';
 import { CART_ADD_ITEM, CART_REMOVE_ITEM } from '../constants/cartConstants';
 
-const addToCart = (productId, qty) => async (dispatch) => {
+const addToCart = (productId, qty) => async (dispatch, getState) => {
   try {
     const { data } = await axios.get('/products/' + productId);
     dispatch({
@@ -14,15 +15,20 @@ const addToCart = (productId, qty) => async (dispatch) => {
         qty
 
       }
-    })
+    });
+    const { cart: { cartItems } } = getState();
+    Cookie.set("cartItems", JSON.stringfy(cartItems));
   } catch (error) {
 
   }
 }
 
-const removeFromCart = (productId) => (dispatch) => {
+const removeFromCart = (productId) => (dispatch, getState) => {
   try {
     dispatch({ type: CART_REMOVE_ITEM, payload: productId });
+
+    const { cart: { cartItems } } = getState();
+    Cookie.set("cartItems", JSON.stringfy(cartItems));
   } catch (error) {
 
   }

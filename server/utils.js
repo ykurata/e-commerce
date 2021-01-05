d1b34a7d6b6c1a0b6d55f184cd20968955f978ba
ykurata <yasukokurata@gmail.com>
2021-01-05 17:02:45 -0500

    added getToken middelware

--- server/utils.js
+++ server/utils.js
@@ -1,5 +1,10 @@
 const jwt = require('jsonwebtoken');
+const config = require('./config');
 
 const getToken = (user) => {
-  return jwt.sign(user, config.JWT_SECRET)
+  return jwt.sign(user, config.JWT_SECRET, {
+    expiresIn: '48h'
+  })
 }
+
+export { getToken }

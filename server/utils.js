8049902b27aed70c97288955a719027640cc3df4
ykurata <yasukokurata@gmail.com>
2021-01-06 17:43:11 -0500

    created isAuth and isAdmin middleware

--- server/utils.js
+++ server/utils.js
@@ -12,4 +12,28 @@ const getToken = (user) => {
   });
 }
 
-module.exports = getToken;
+const isAuth = (req, res, next) => {
+  const token = req.headers.authorization;
+  if (token) {
+    const onlyToken = token.slice(7, token.length);
+    jwt.verify(onlyToken, config.JWT_SECRET, (err, decode) => {
+      if (err) {
+        return res.status(401).send({ msg: 'Invalid Token' });
+      }
+      res.user = token;
+      return next();
+    })
+  }
+  return res.status(401).send({ msg: 'Token is not supplied' });
+}
+
+const isAdmin = (req, res, next) => {
+  if (req.user && req.user.isAdmin) {
+    return next();
+  }
+  return res.status(401).send({ msg: 'Admin Token is not valid' });
+}
+
+
+
+module.exports = getToken, isAuth, isAdmin;

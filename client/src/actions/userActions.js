3c81b27a5a7f60558f8620f731b244c3bfa1fd85
ykurata <yasukokurata@gmail.com>
2021-01-06 15:47:53 -0500

    created register constants

--- client/src/actions/userActions.js
+++ client/src/actions/userActions.js
@@ -1,6 +1,7 @@
 import axios from 'axios';
 import Cookie from 'js-cookie';
 import { USER_SIGNIN_REQUEST, USER_SIGNIN_SUCCESS, USER_SIGNIN_FAIL } from '../constants/userConstants';
+import { USER_REGISTER_REQUEST, USER_REGISTER_SUCCESS, USER_REGISTER_FAIL } from '../constants/userConstants';
 
 const signin = (email, password) => async (dispatch) => {
   dispatch({ type: USER_SIGNIN_REQUEST, payload: { email, password } });
@@ -16,8 +17,8 @@ const signin = (email, password) => async (dispatch) => {
 const register = (name, email, password) => async (dispatch) => {
   dispatch({ type: USER_REGISTER_REQUEST, payload: { name, email, password } });
   try {
-    const { data } = await axios.post('/user/register', { email, password });
-    dispatch({ type: USER_REGISTER_REQUEST, payload: data });
+    const { data } = await axios.post('/user/register', { name, email, password });
+    dispatch({ type: USER_REGISTER_SUCCESS, payload: data });
     Cookie.set('userInfo', JSON.stringify(data));
   } catch (error) {
     dispatch({ type: USER_REGISTER_FAIL, payload: error.message });

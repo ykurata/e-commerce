4bc462a2e4ecbd4cb518d1d580f8e424876a695d
ykurata <yasukokurata@gmail.com>
2021-01-07 15:18:25 -0500

    Added create product route

--- server/routes/product.js
+++ server/routes/product.js
@@ -1,32 +1,30 @@
 const express = require("express");
 const router = express.Router();
-const auth = require("./utils/auth");
-//const isAdmin = require("./utils/isAdmin");
+const { getToken } = require('../utils');
 const Product = require("../models/Product");
 
 router.get("/", async (req, res) => {
   const products = await Product.find({});
-  if (products) {
-    return res.status(200).json(products);
-  }
-  return res.status(404).json({ error: "Product not found" });
+  return res.send(products);
 });
 
-// router.post("/", auth, isAdmin, async (req, res) => {
-//   const product = new Product({
-//     name: req.body.name,
-//     price: req.body.price,
-//     image: req.body.image,
-//     brand: req.body.brand,
-//     category: req.body.category,
-//     countInStock: req.body.countInStock,
-//     description: req.body.description,
-//   });
-//   const newProduct = await product.save();
-//   if (newProduct) {
-//     return res.status(200).json(newProduct);
-//   }
-//   return res.status(500).json({ error: "Error in creating a product" });
-// });
+router.post('/', async (req, res) => {
+  const product = new Product({
+    name: req.body.name,
+    price: req.body.price,
+    image: req.body.image,
+    brand: req.body.brand,
+    category: req.body.category,
+    countInStock: req.body.countInStock,
+    description: req.body.description,
+    rating: req.body.rating,
+    numReviews: req.body.numReviews,
+  });
+  const newProduct = await product.save();
+  if (newProduct) {
+    return res.status(200).send({ message: 'New Product Created', data: newProduct });
+  }
+  return res.status(500).send({ message: 'Error in Creating Product' });
+});
 
 module.exports = router;
